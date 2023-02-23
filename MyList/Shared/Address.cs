c2bbb4bfcfc0f92e8b9f233c6acc24cfee7db4be
Минаев Александр <tenshisasha@gmail.com>
2023-02-23 16:18:49 +0300

    Fixed table rendering on page.

--- MyList/Shared/Address.cs
+++ MyList/Shared/Address.cs
@@ -7,10 +7,12 @@ namespace MyList.Shared
     public class Address
     {
         public int Id { get; set; }
+
         [Required]
         public DateTime DateMeasurment { get; set; }
 
         public DateTime DateMontage { get; set; }
+
         [Required]
         [MaxLength(200)]
         public string Name { get; set; }
